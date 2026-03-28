63f1fa73fc48a761905747ffdc721acd5a90e2f9
C WX <c15814506207@gmail.com>
2026-03-29 03:30:54 +0800

    fix: import Link in StitchWorkspaceShell

--- src/components/workspace/StitchWorkspaceShell.jsx
+++ src/components/workspace/StitchWorkspaceShell.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import '../../styles/homePage.css'
 import { fetchUserProductsForGallery } from '../../services/productGallery'
 import {
